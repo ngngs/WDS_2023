5c22cbdd17eabf52f6d7e9e0dd814faf4fdbcd94
djdjdddd <djdj2297@gmail.com>
2023-12-26 23:07:17 +0900

    feat: 김용희 46일차 - 아이템 1. 완벽 공략 5 - 리플렉션

---
+++ Day40~49/Day46/day46_yh/ReflectionTest.java
@@ -0,0 +1,17 @@
+package study.my.java.whiteship.effective_java.chapter01.item01.lecture10;
+
+public class ReflectionTest {
+
+    // field
+    private String field1;
+
+    // constructor
+    public ReflectionTest(String field1){
+        this.field1 = field1;
+    }
+
+    // method
+    public void method1(){
+        System.out.println("ReflectionTest의 메서드1 입니다.");
+    }
+}
